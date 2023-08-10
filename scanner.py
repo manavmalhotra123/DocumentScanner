2cde1e964e822cee4c9d3263b32cee0c9c1879e8
manavmalhotra123 <mmalhotra_be21@thapar.edu>
2023-08-10 11:05:19 +0530

    done

--- scanner.py
+++ scanner.py
@@ -1,14 +1,78 @@
-import cv2 
-import numpy as np 
-import utils 
+import cv2
+import numpy as np
+import utlis
 
-# setting up the camera
-camera = cv2.VideoCapture(0)
-# fixing the resolution
-camera.set(10,160)
 
-heightImage = 540
-WidthImage  = 480
 
-utils.initializeTrackbars()
-count = 1
+cap = cv2.VideoCapture(1)
+cap.set(10,160)
+heightImg = 540
+widthImg = 480
+
+
+utlis.initializeTrackbars()
+count=1
+
+while True:
+
+    success, img = cap.read()
+    img = cv2.resize(img, (widthImg, heightImg))
+    imgBlank = np.zeros((heightImg,widthImg, 3), np.uint8)
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1)
+    thres=utlis.valTrackbars()
+    imgThreshold = cv2.Canny(imgBlur,thres[0],thres[1])
+    kernel = np.ones((5, 5))
+    imgDial = cv2.dilate(imgThreshold, kernel, iterations=2)
+    imgThreshold = cv2.erode(imgDial, kernel, iterations=1)
+
+    ## FIND ALL COUNTOURS
+    imgContours = img.copy() # COPY IMAGE FOR DISPLAY PURPOSES
+    imgBigContour = img.copy() # COPY IMAGE FOR DISPLAY PURPOSES
+    contours, hierarchy = cv2.findContours(imgThreshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(imgContours, contours, -1, (0, 255, 0), 10)
+
+
+    biggest, maxArea = utlis.biggestContour(contours)
+    if biggest.size != 0:
+        biggest=utlis.reorder(biggest)
+        cv2.drawContours(imgBigContour, biggest, -1, (0, 255, 0), 20)
+        imgBigContour = utlis.drawRectangle(imgBigContour,biggest,2)
+        pts1 = np.float32(biggest) # PREPARE POINTS FOR WARP
+        pts2 = np.float32([[0, 0],[widthImg, 0], [0, heightImg],[widthImg, heightImg]])
+        matrix = cv2.getPerspectiveTransform(pts1, pts2)
+        imgWarpColored = cv2.warpPerspective(img, matrix, (widthImg, heightImg))
+
+        imgWarpColored=imgWarpColored[20:imgWarpColored.shape[0] - 20, 20:imgWarpColored.shape[1] - 20]
+        imgWarpColored = cv2.resize(imgWarpColored,(widthImg,heightImg))
+
+        imgWarpGray = cv2.cvtColor(imgWarpColored,cv2.COLOR_BGR2GRAY)
+        imgAdaptiveThre= cv2.adaptiveThreshold(imgWarpGray, 255, 1, 1, 7, 2)
+        imgAdaptiveThre = cv2.bitwise_not(imgAdaptiveThre)
+        imgAdaptiveThre=cv2.medianBlur(imgAdaptiveThre,3)
+
+        imageArray = ([img,imgThreshold,imgContours],
+                      [imgBigContour,imgWarpColored,imgAdaptiveThre])
+
+    else:
+        imageArray = ([img,imgThreshold,imgContours],
+                      [imgBlank, imgBlank, imgBlank])
+
+    lables = [["Original","Threshold","Contours"],
+              ["Biggest Contour","Warp Prespective","Adaptive Threshold"]]
+
+    stackedImage = utlis.stackImages(imageArray,0.75,lables)
+    cv2.imshow("Result",stackedImage)
+
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        cv2.imwrite("Scanned/myImage"+str(count)+".jpg",imgWarpColored)
+        cv2.rectangle(stackedImage, ((int(stackedImage.shape[1] / 2) - 230), int(stackedImage.shape[0] / 2) + 50),
+                      (1100, 350), (0, 255, 0), cv2.FILLED)
+        cv2.putText(stackedImage, "Scan Saved", (int(stackedImage.shape[1] / 2) - 200, int(stackedImage.shape[0] / 2)),
+                    cv2.FONT_HERSHEY_DUPLEX, 3, (0, 0, 255), 5, cv2.LINE_AA)
+        cv2.imshow('Result', stackedImage)
+        cv2.waitKey(300)
+        count += 1
+
+    elif cv2.waitKey(1) & 0xFF == ord('q'):
+        break
